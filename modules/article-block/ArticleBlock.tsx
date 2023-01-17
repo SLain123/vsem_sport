1abbb1acb5a6b5afecdcb827c53103110e985b59
Artem <sl163@ya.ru>
2023-01-17 16:40:43 +0300

    Adds new fonts; Adds redesign for header and footer; Adds a little fixes for dev stub; Changes logo;

---
+++ modules/article-block/ArticleBlock.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+import styles from "./ArticleBlock.module.scss";
+
+const ArticleBlock: FC = () => {
+  return <div className={styles.artl_container}>ArticleBlock</div>;
+};
+
+export { ArticleBlock };
