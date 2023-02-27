5a10180ef8fe3d93868bd9b80063e413cb6078a3
Artem <sl163@ya.ru>
2023-02-27 13:08:15 +0300

    Adds PWA functional for web site;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,30 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { Provider } from "react-redux";
 import { AppProps } from "next/app";
 import { wrapper } from "redux/store";
 
 const MyApp: FC<AppProps> = ({ Component, ...rest }) => {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, []);
+
   const { store, props } = wrapper.useWrappedStore(rest);
   return (
     <Provider store={store}>
