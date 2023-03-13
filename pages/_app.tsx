3f3ff5cb842bb3bc7c1e9ccfb85d64a8b7bff67c
Artem <sl163@ya.ru>
2023-03-13 10:53:28 +0300

    Removes condition from servise worker launch;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,19 +16,17 @@ const MyApp: FC<AppProps> = ({ Component, ...rest }) => {
   useEffect(() => {
     console.log("serviceWorker in", navigator);
     if ("serviceWorker" in navigator) {
-      window.addEventListener("load", function () {
-        navigator.serviceWorker.register("/sw.js").then(
-          function (registration) {
-            console.log(
-              "Service Worker registration successful with scope: ",
-              registration.scope
-            );
-          },
-          function (err) {
-            console.log("Service Worker registration failed: ", err);
-          }
-        );
-      });
+      navigator.serviceWorker.register("/sw.js").then(
+        function (registration) {
+          console.log(
+            "Service Worker registration successful with scope: ",
+            registration.scope
+          );
+        },
+        function (err) {
+          console.log("Service Worker registration failed: ", err);
+        }
+      );
     }
   }, []);
 
